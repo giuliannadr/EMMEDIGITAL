6af7cc20df68e30fbab57d78fca8c0292ba2097f
giuliannadr <giulianadirocco1@gmail.com>
2026-04-11 11:16:47 -0300

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smooth: true,
+      smoothWheel: true,
     });
 
     function raf(time: number) {
